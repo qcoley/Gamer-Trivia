9cccd9ef675b1bccd535ec22ca8cd669d21466cb
Quinton Coley <qcoley@uab.edu>
2021-03-25 18:59:26 -0500

    Added a simple logo to start screen, as well as a pause menu (screen) that can be accesed from the game view. You can return to the game, or exit back to the start screen.

---
+++ GamerTrivia/GamerTrivia/controllers/MenuViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MenuViewController.swift
+//  GamerTrivia
+//
+//  Created by Student Account  on 3/25/21.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
